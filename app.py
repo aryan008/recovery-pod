12008626ba4dd6548d11b958ec5a15b55100666e
aryan008 <adam.s.ryan@pwc.com>
2021-07-02 08:58:13 +0000

    Add: copying of Try statement querying and total and add to IndexError block for new entry

--- app.py
+++ app.py
@@ -389,6 +389,88 @@ def new_entry():
             # call the get_result() function
             result = get_result(username)
 
+            # get the choices made by the user on their entry form
+            final_attributes = request.form.getlist("options.choice")
+            # create the total counter
+            total = 0
+
+            # set up the attribute queries to get each individual response
+            attr_1_query = final_attributes[0]
+            attr_2_query = final_attributes[1]
+            attr_3_query = final_attributes[2]
+            attr_4_query = final_attributes[3]
+            attr_5_query = final_attributes[4]
+            attr_6_query = final_attributes[5]
+            attr_7_query = final_attributes[6]
+            attr_8_query = final_attributes[7]
+
+            # The below if statements match the user's responses on an 
+            # attribute-by-attribute basis and get the correct values from the constant dictionaries.
+            # The resulting score is then added to the above total counter
+            if attr_1_query == list(ATTRIBUTE_1_DICT.keys())[0]:
+                attr_1_result = list(ATTRIBUTE_1_DICT.values())[0]
+            elif attr_1_query == list(ATTRIBUTE_1_DICT.keys())[1]:
+                attr_1_result = list(ATTRIBUTE_1_DICT.values())[1]
+            elif attr_1_query == list(ATTRIBUTE_1_DICT.keys())[2]:
+                attr_1_result = list(ATTRIBUTE_1_DICT.values())[2]
+            total += attr_1_result
+            
+            if attr_2_query == list(ATTRIBUTE_2_DICT.keys())[0]:
+                attr_2_result = list(ATTRIBUTE_2_DICT.values())[0]
+            elif attr_2_query == list(ATTRIBUTE_2_DICT.keys())[1]:
+                attr_2_result = list(ATTRIBUTE_2_DICT.values())[1]
+            elif attr_2_query == list(ATTRIBUTE_2_DICT.keys())[2]:
+                attr_2_result = list(ATTRIBUTE_2_DICT.values())[2]
+            total += attr_2_result
+
+            if attr_3_query == list(ATTRIBUTE_3_DICT.keys())[0]:
+                attr_3_result = list(ATTRIBUTE_3_DICT.values())[0]
+            elif attr_3_query == list(ATTRIBUTE_3_DICT.keys())[1]:
+                attr_3_result = list(ATTRIBUTE_3_DICT.values())[1]
+            elif attr_3_query == list(ATTRIBUTE_3_DICT.keys())[2]:
+                attr_3_result = list(ATTRIBUTE_3_DICT.values())[2]
+            total += attr_3_result
+
+            if attr_4_query == list(ATTRIBUTE_4_DICT.keys())[0]:
+                attr_4_result = list(ATTRIBUTE_4_DICT.values())[0]
+            elif attr_4_query == list(ATTRIBUTE_4_DICT.keys())[1]:
+                attr_4_result = list(ATTRIBUTE_4_DICT.values())[1]
+            elif attr_4_query == list(ATTRIBUTE_4_DICT.keys())[2]:
+                attr_4_result = list(ATTRIBUTE_4_DICT.values())[2]
+            total += attr_4_result
+
+            if attr_5_query == list(ATTRIBUTE_5_DICT.keys())[0]:
+                attr_5_result = list(ATTRIBUTE_5_DICT.values())[0]
+            elif attr_5_query == list(ATTRIBUTE_5_DICT.keys())[1]:
+                attr_5_result = list(ATTRIBUTE_5_DICT.values())[1]
+            elif attr_5_query == list(ATTRIBUTE_5_DICT.keys())[2]:
+                attr_5_result = list(ATTRIBUTE_5_DICT.values())[2]
+            total += attr_5_result
+
+            if attr_6_query == list(ATTRIBUTE_6_DICT.keys())[0]:
+                attr_6_result = list(ATTRIBUTE_6_DICT.values())[0]
+            elif attr_6_query == list(ATTRIBUTE_6_DICT.keys())[1]:
+                attr_6_result = list(ATTRIBUTE_6_DICT.values())[1]
+            elif attr_6_query == list(ATTRIBUTE_6_DICT.keys())[2]:
+                attr_6_result = list(ATTRIBUTE_6_DICT.values())[2]
+            total += attr_6_result
+
+            if attr_7_query == list(ATTRIBUTE_7_DICT.keys())[0]:
+                attr_7_result = list(ATTRIBUTE_7_DICT.values())[0]
+            elif attr_7_query == list(ATTRIBUTE_7_DICT.keys())[1]:
+                attr_7_result = list(ATTRIBUTE_7_DICT.values())[1]
+            elif attr_7_query == list(ATTRIBUTE_7_DICT.keys())[2]:
+                attr_7_result = list(ATTRIBUTE_7_DICT.values())[2]
+            total += attr_7_result
+
+            if attr_8_query == list(ATTRIBUTE_8_DICT.keys())[0]:
+                attr_8_result = list(ATTRIBUTE_8_DICT.values())[0]
+            elif attr_8_query == list(ATTRIBUTE_8_DICT.keys())[1]:
+                attr_8_result = list(ATTRIBUTE_8_DICT.values())[1]
+            elif attr_8_query == list(ATTRIBUTE_8_DICT.keys())[2]:
+                attr_8_result = list(ATTRIBUTE_8_DICT.values())[2]
+            total += attr_8_result
+
 
 # function to get the date of the last entry by the user
 def get_date(username):
