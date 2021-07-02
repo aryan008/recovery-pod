22d92c852abbe50cebe0d54932632c6fe39ccc7a
aryan008 <adam.s.ryan@pwc.com>
2021-07-02 06:56:02 +0000

    Add: Initialise Flask app and test

--- app.py
+++ app.py
@@ -10,3 +10,19 @@ import json
 from werkzeug.security import generate_password_hash, check_password_hash
 if os.path.exists("env.py"):
     import env
+
+
+
+
+# Initialise flask and wire up Mongo DB
+app = Flask(__name__)
+
+# Route for the home page
+@app.route("/")
+def hello():
+    return "Hello"
+
+if __name__ == "__main__":
+    app.run(host=os.environ.get("IP"),
+            port=int(os.environ.get("PORT")),
+            debug=True)
