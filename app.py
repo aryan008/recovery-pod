14e687466a6281e1c3f62687a12b8bd3f3585d3f
aryan008 <adam.s.ryan@pwc.com>
2021-07-02 08:34:12 +0000

    Add: code for login app route added to app.py

--- app.py
+++ app.py
@@ -74,6 +74,37 @@ def create_account():
     return render_template("create_account.html")
 
 
+# Route for the login page
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        # check if username exists in db
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+
+        # if the username is on the DB
+        if existing_user:
+            # ensure hashed password matches user input
+            if check_password_hash(
+                    existing_user["password"], request.form.get("password")):
+                        session["user"] = request.form.get("username").lower()
+                        flash("Welcome, {}".format(
+                            request.form.get("username")))
+                        return redirect(url_for(
+                            "profile", username=session["user"]))
+            else:
+                # invalid password match
+                flash("Incorrect Username and/or Password")
+                return redirect(url_for("login"))
+
+        else:
+            # username doesn't exist
+            flash("Incorrect Username and/or Password")
+            return redirect(url_for("login"))
+
+    return render_template("login.html")
+
+
 # Route for the profile page
 @app.route("/profile/<username>", methods=["GET", "POST"])
 def profile(username):
